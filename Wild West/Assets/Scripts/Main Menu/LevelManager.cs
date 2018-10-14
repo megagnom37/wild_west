081966af74cccaab4c5256885cd9de77de704126
Ivan Skripachev <skripachevivan@gmail.com>
2018-10-14 15:29:13 +0300

    Added init files and main menu and single player menu

---
+++ Wild West/Assets/Scripts/Main Menu/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void OnSinglePlayerClick()
+    {
+        SceneManager.LoadScene("single_player_menu");
+    }
+
+    public void OnMultiPlayerClick()
+    {
+        SceneManager.LoadScene("multi_player_menu");
+    }
+
+    public void OnSettingsClick()
+    {
+        SceneManager.LoadScene("settings");
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Wild West/Assets/Scripts/SingleMenu/SingleMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SingleMenuManager : MonoBehaviour {
+
+    public void OnMainMenuClick()
+    {
+        SceneManager.LoadScene("main_menu");
+    }
+
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("single_player_game");
+    }
+}
