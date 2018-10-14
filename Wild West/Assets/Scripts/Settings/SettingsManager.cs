041ec4dce67ca12c10eefbb2c88ab9b9f64d4d68
saha2231 <saha_97@bk.ru>
2018-10-14 15:51:19 +0300

    added Multiplayer and Settings menu

---
+++ Wild West/Assets/Scripts/Settings/SettingsManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour {
+
+    public void OnMainMenuClick()
+    {
+        SceneManager.LoadScene("main_menu");
+    }
+}
