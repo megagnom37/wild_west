7763b17b644ef46dee4845a25881e36a565ee505
Ivan Skripachev <skripachevivan@gmail.com>
2018-10-25 23:58:57 +0300

    Added About in settings

---
+++ Wild West/Assets/Scripts/Settings/AboutVersionManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AboutVersionManager : MonoBehaviour {
+    Text version;
+
+    void Awake(){
+        version = GetComponent<Text>();
+        version.text = Application.version;
+    }
+}
