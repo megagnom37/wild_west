f42a1f24b55882f2b0344724208c79d16fe5e55e
Alexander <alexander.antonov.1997@gmail.com>
2018-11-05 13:49:53 +0300

    refactoring multi_player_develop

--- Wild West/Assets/Scripts/MultiGame/Character/MultiCharacterController.cs
+++ Wild West/Assets/Scripts/MultiGame/Character/MultiCharacterController.cs
@@ -9,13 +9,10 @@ public class MultiCharacterController : NetworkBehaviour {
     private float _endGameTime = 0.0f;
     public float _resultGameTime = 0.0f;
     public bool _shootIsDone = false;
-    // private ClickManager _clickManager;
 
-    // Use this for initialization
     void Start () {
-       // _clickManager = new ClickManager();
         this.gameObject.transform.position = new Vector3(6, 1.5f, -1);
-        if (isLocalPlayer)
+        if (this.isLocalPlayer)
         {
             this.gameObject.transform.position = new Vector3(-6, 1.5f, -1);
             this.name = "HostPlayer";
@@ -24,50 +21,38 @@ public class MultiCharacterController : NetworkBehaviour {
         {
             this.name = "ClientPlayer";
         }
-
     }
 
-    [Command(channel = 0)]
+    [Command(channel = 0)] // Server
     public void CmdMyStartGame()
     {
-        //StartCoroutine(LoadScene());
-        if (isServer)
             RpcStartGame();
     }
 
-    // Так как серверы также являются клиентами!!!
-    [ClientRpc(channel = 0)]
+    [ClientRpc(channel = 0)] // Client
     void RpcStartGame()
     {
-        if (this.isClient && this.isLocalPlayer) //(this.isClient)
+        if (this.isLocalPlayer)
         {
-            StartCoroutine(LoadScene());
+            ClickToStart();
         }
     }
 
-    [Command(channel = 0)]
+    [Command(channel = 0)] // Server
     void CmdSetResultTime(float res)
     {
-        if (isServer)
             _resultGameTime = res;
     }
 
-    IEnumerator LoadScene()
+    public void ClickToStart()
     {
-        print("StartLoadScene");
-        yield return new WaitForSeconds(0);
-        print("Tap to start"); //??????????
-        GameObject.Find("Txt").GetComponent<TextMesh>().text = "Tap to start";
-        ClickManager.OnClicked += clickToStart;
-    }
-
-    void clickToStart()
-    {
-        float timeForStartGame = 5.0f; // + Random.Range(-2.0f, 2.0f)
-        print("Time to start: " + timeForStartGame.ToString());
-        ClickManager.OnClicked -= clickToStart;
-        ClickManager.OnClicked += this.ClickToShot;
-        StartCoroutine(StartGameAfterSeconds(timeForStartGame));
+        if (isClient)
+        {
+            float timeForStartGame = 5.0f; // + Random.Range(-2.0f, 2.0f)
+            print("Time to start: " + timeForStartGame.ToString());
+            ClickManager.OnClicked += this.ClickToShot;
+            StartCoroutine(StartGameAfterSeconds(timeForStartGame));
+        }
     }
 
     IEnumerator StartGameAfterSeconds(float timeInSec)
@@ -79,7 +64,6 @@ public class MultiCharacterController : NetworkBehaviour {
         print("Go");
         GameObject.Find("Txt").GetComponent<TextMesh>().text = "Go";
 
-        //StartCoroutine(ResultGameAfterSeconds(3.0f));
     }
 
     public void ClickToShot()
@@ -99,28 +83,4 @@ public class MultiCharacterController : NetworkBehaviour {
         _shootIsDone = true;
     }
 
-
-    /*
-    IEnumerator ResultGameAfterSeconds(float timeInSec)
-    {
-        yield return new WaitForSeconds(timeInSec);
-
-        float resultTime = _player.ShootTime - _startGameTime;
-        print(resultTime);
-        print(_enemy.getShootTime());
-        _resultPanel.SetActive(true);
-        if ((resultTime > 0.0f) && (resultTime < _enemy.getShootTime()))
-        {
-            print("Win!");
-            _resultPanel.GetComponent<ResultPanelManager>().SetResult("WIN", resultTime);
-            _enemy.gameObject.transform.Rotate(new Vector3(90, 0, 0));
-        }
-        else
-        {
-            print("Lose!");
-            _resultPanel.GetComponent<ResultPanelManager>().SetResult("LOSE", resultTime);
-            _player.gameObject.transform.Rotate(new Vector3(90, 0, 0));
-        }
-    }
-    */
 }
--- Wild West/Assets/Scripts/MultiGame/Game/MultiPlayerController.cs
+++ Wild West/Assets/Scripts/MultiGame/Game/MultiPlayerController.cs
@@ -6,111 +6,47 @@ using UnityEngine.UI;
 
 public class MultiPlayerController : NetworkBehaviour
 {
-    // print(this.GetComponent<NetworkManager>().numPlayers);
+ 
     [SerializeField] private TextMesh _text;
 
     private bool flag1= true;
     private bool flag2 = true;
-   // private MultiCharacterController player1;
-    //private MultiCharacterController player2;
-
-
+    private MultiCharacterController player1;
+    private MultiCharacterController player2;
 
     private void Update()
     {
-        // ВЫПОЛНЯЕТСЯ ТОЛЬКО НА СЕРВЕРЕ ПО ДЕФОЛТУ
-       if ((this.GetComponent<NetworkManager>().numPlayers == 2) && flag1)
+        if ((this.GetComponent<NetworkManager>().numPlayers == 2) && flag1)
         {
             flag1 = false;
-            //player1 = GameObject.Find("LocalPlayer").GetComponent<MultiCharacterController>();
-            //player2 = GameObject.Find("NotLocalPlayer").GetComponent<MultiCharacterController>();
-            GameObject.Find("HostPlayer").GetComponent<MultiCharacterController>().CmdMyStartGame();
-            //print("Hello22222");
-            GameObject.Find("ClientPlayer").GetComponent<MultiCharacterController>().CmdMyStartGame();
+            player1 = GameObject.Find("HostPlayer").GetComponent<MultiCharacterController>();
+            player2 = GameObject.Find("ClientPlayer").GetComponent<MultiCharacterController>();
 
-            //player1.name = "CHANGE";
-            //player2.name = "CHANGE";
+            player1.CmdMyStartGame();
+            player2.CmdMyStartGame();
         }
-       // ОТРЕФАКТОРИТЬ (код чтобы просто работало)
-       if (this.GetComponent<NetworkManager>().numPlayers == 2)
-       if (GameObject.Find("HostPlayer").GetComponent<MultiCharacterController>()._shootIsDone &&
-            GameObject.Find("ClientPlayer").GetComponent<MultiCharacterController>()._shootIsDone && flag2)
+
+        if (this.GetComponent<NetworkManager>().numPlayers == 2)
         {
-            flag2 = false;
-            if (GameObject.Find("HostPlayer").GetComponent<MultiCharacterController>()._resultGameTime < 
-                GameObject.Find("ClientPlayer").GetComponent<MultiCharacterController>()._resultGameTime)
+            if (player1._shootIsDone && player2._shootIsDone && flag2)
             {
-                    print(GameObject.Find("HostPlayer").GetComponent<MultiCharacterController>()._resultGameTime);
-                    print(GameObject.Find("ClientPlayer").GetComponent<MultiCharacterController>()._resultGameTime);
+                flag2 = false;
+                if (player1._resultGameTime < player2._resultGameTime)
+                {
+                    print(player1._resultGameTime);
+                    print(player2._resultGameTime);
                     print("WIN PLAYER1");
-                _text.text = "WIN PLAYER1";
-            }
-            else
-            {
-                    print(GameObject.Find("HostPlayer").GetComponent<MultiCharacterController>()._resultGameTime);
-                    print(GameObject.Find("ClientPlayer").GetComponent<MultiCharacterController>()._resultGameTime);
+                    _text.text = "WIN PLAYER1";
+                }
+                else
+                {
+                    print(player1._resultGameTime);
+                    print(player2._resultGameTime);
                     print("WIN PLAYER2");
-                _text.text = "WIN PLAYER2";
+                    _text.text = "WIN PLAYER2";
+                }
             }
         }
     }
-    /*
-    IEnumerator LoadScene()
-    {
-        print("StartLoadScene");
-        yield return new WaitForSeconds(2);
-        print("Tap to start");
-        ClickManager.OnClicked += clickToStart;
-    }
-    
-    private float _startGameTime = 0.0f;
-    [SerializeField] private GameObject _resultPanel;
 
-    // Use this for initialization
-    void Start () {
-        StartCoroutine(LoadScene());
-    }
-
-
-    void clickToStart()
-    {
-        float timeForStartGame = 5.0f + Random.Range(-2.0f, 2.0f);
-        print("Time to start: " + timeForStartGame.ToString());
-        ClickManager.OnClicked -= clickToStart;
-        ClickManager.OnClicked += _player.ClickToShot;
-        StartCoroutine(StartGameAfterSeconds(timeForStartGame));
-    }
-
-    IEnumerator StartGameAfterSeconds(float timeInSec)
-    {
-        print("Ready");
-        yield return new WaitForSeconds(timeInSec);
-        _startGameTime = Time.time;
-        print("Go");
-
-        StartCoroutine(ResultGameAfterSeconds(3.0f));
-    }
-
-    IEnumerator ResultGameAfterSeconds(float timeInSec)
-    {
-        yield return new WaitForSeconds(timeInSec);
-
-        float resultTime = _player.ShootTime - _startGameTime;
-        print(resultTime);
-        print(_enemy.getShootTime());
-        _resultPanel.SetActive(true);
-        if ((resultTime > 0.0f) && (resultTime < _enemy.getShootTime()))
-        {
-            print("Win!");
-            _resultPanel.GetComponent<ResultPanelManager>().SetResult("WIN", resultTime);
-            _enemy.gameObject.transform.Rotate(new Vector3(90, 0, 0));
-        }
-        else
-        {
-            print("Lose!");
-            _resultPanel.GetComponent<ResultPanelManager>().SetResult("LOSE", resultTime);
-            _player.gameObject.transform.Rotate(new Vector3(90, 0, 0));
-        }
-    }
-    */
 }
