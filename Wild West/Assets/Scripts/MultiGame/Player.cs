982797fc0ba6ccc10061a7d6d5be5c98f5ffd9b1
Ivan Skripachev <skripachevivan@gmail.com>
2018-11-05 13:45:05 +0300

    Added multiplayer game mode

---
+++ Wild West/Assets/Scripts/MultiGame/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Player : NetworkBehaviour 
+{
+	[SyncVar] private float resultTime = 0.0f;
+	[SyncVar] public bool isResultReady = false;
+	[SyncVar] public string resultMatch;
+	public bool isPlayer = false;
+
+	private GameObject resultPanel;
+	private Text resultText;
+
+	void Start()
+	{
+		if (isLocalPlayer) {
+			resultPanel = GameObject.Find ("Panel");
+			resultText = GameObject.Find ("Text").GetComponent<Text>();
+			resultPanel.SetActive (false);
+		}
+	}
+
+	public void ShowResults()
+	{
+		if (isResultReady && isPlayer) {
+			transform.name = "Winner: " + resultTime.ToString();
+			resultText.text = resultMatch;
+			resultPanel.SetActive (true);
+		}
+	}
+
+	public void SetResult(float time)
+	{
+		resultTime = time;
+		GameController.SetPlayerTime (name, resultTime);
+	}
+}
---
+++ Wild West/Assets/Scripts/MultiGame/Shoot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Shoot : NetworkBehaviour {
+
+	[SyncVar] private float startShotTime = 0.0f;
+	private float startTime = 0.0f;
+	private float tapToShot = 0.0f;
+	private bool isShotWas = false;
+
+	public void SetStartShotTime(float time)
+	{
+		if (isServer) {
+			startShotTime = time;
+			print ("SetStartShotTime.isServer");
+		}
+	}
+
+	public void startProc()
+	{
+		StartCoroutine (SleepToStart());
+	}
+
+	IEnumerator SleepToStart()
+	{
+		print ("startShotTime: " + startShotTime.ToString ());
+		yield return new WaitForSeconds (startShotTime);
+		startTime = Time.time;
+		print ("startTime: " + startTime.ToString ());
+		StartCoroutine (Sleep5Sec());
+	}
+
+	IEnumerator Sleep5Sec()
+	{
+		if (isServer)
+			yield return new WaitForSeconds (4);
+		else 
+			yield return new WaitForSeconds (5);
+		if (isServer)
+			GameController.ProcResults ();
+
+		Player player = GameController.GetPlayer (name);
+		player.ShowResults ();
+	}
+		
+	void Update () 
+	{
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetKeyDown(KeyCode.Space) && !isShotWas) {
+			isShotWas = true;
+			Click ();
+		}
+	}
+
+	[Client]
+	void Click()
+	{
+		tapToShot = Time.time;
+		print ("tapToShot: " + tapToShot.ToString ());
+		Player player = GameController.GetPlayer (transform.name);
+		player.isPlayer = true;
+		if (startTime != 0.0f)
+			CmdWasShoot (transform.name, tapToShot - startTime);
+		else
+			CmdWasShoot (transform.name, -1);
+	}
+
+	[Command]
+	void CmdWasShoot(string name, float time)
+	{
+		Player player = GameController.GetPlayer (name);
+		player.SetResult (time);
+		Debug.Log (name + " : " + (time).ToString());
+	}
+}
