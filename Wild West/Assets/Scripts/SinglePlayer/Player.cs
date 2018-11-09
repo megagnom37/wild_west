17ee6661d233dc6ff3601c792048febe4ee3118f
Ivan Skripachev <skripachevivan@gmail.com>
2018-11-09 22:00:37 +0300

    Create test animation branch

---
+++ Wild West/Assets/Scripts/SinglePlayer/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Animator _animator;
+    public GameObject leg;
+    public GameObject hand;
+    public GameObject gun;
+
+    // Use this for initialization
+    void Start () {
+        _animator = GetComponent<Animator>();
+        gun.transform.parent = leg.transform;
+    }
+	
+	public void SetHandAsParent()
+    {
+        gun.transform.parent = hand.transform;
+    }
+}
