daed1e28d328dc2472208be81a2e904e22ec18a0
Ivan Skripachev <skripachevivan@gmail.com>
2018-11-18 14:56:37 +0300

    Integrated Multi Menu. Fixed bugs. Added animations. Updated prefabs

---
+++ Wild West/Assets/Scripts/MultiMenu/AnimSitCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimSitCharacter : MonoBehaviour {
+
+    Animation animat;
+    public float delayBetweenAnimation = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        animat = GetComponent<Animation>();
+        StartCoroutine(SitPlay());
+    }
+
+    IEnumerator SitPlay()
+    {
+        animat.Play();
+        yield return new WaitForSeconds(delayBetweenAnimation);
+        StartCoroutine(SitPlay());
+    }
+}
